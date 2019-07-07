9976295d0ee96430c24b8ae13ac926118744d0c9
Williams Rivas <williams.rivas1@gmail.com>
2019-07-07 12:38:43 -0400

    se utiliza promise.all para acelerar descarga

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const rp = require("request-promise");
 const fs = require('fs').promises;
 const uuidv1 = require('uuid/v1');
 
-const lastvideo = 18272;
+const lastvideo = 17034;
 
 function diff_minutes(dt2, dt1) {
   var diff = (dt2.getTime() - dt1.getTime()) / 1000;
@@ -15,31 +15,39 @@ async function download() {
   let url = 'https://elrellano.com/_ficheros/';
   let next = true;
   let i = lastvideo;
+  let downloads = [];
   while (next) {
     if (i > 0) {
-      console.log(`donwload video ${i}`)
       let initDate = new Date();
-      await rp({
-        url: url.concat(`items_${i}_mp4.mp4`),
+      let filename = `items_${i}_mp4.mp4`;
+      let promise = rp({
+        url: url.concat(filename),
         method: 'GET',
         encoding: null,
         headers: {
           'Content-type': 'video/mp4'
         }
       }).then((data) => {
-        return fs.writeFile(`/home/williams/videos/rellano/items_${i}_mp4.mp4`, data, function (err) {
+        return fs.writeFile(`/home/williams/videos/rellano/${filename}`, data, function (err) {
           console.log(err);
         });
       }).catch(function (err) {
-        console.log(`catch error items_${i}_mp4.mp4`);
+        return Promise.resolve({
+          error: filename
+        });
       });
+      downloads.push(promise);
       const diff = diff_minutes(new Date(), initDate);
-      console.log(`duration ${diff}`)
-      i--;
     } else {
       next = !next;
     }
+    i--;
   }
+  Promise.all(downloads).then(values => {
+    for (let j = 0; j <= values.length; j++) {
+      console.log(values[j])
+    }
+  });
 }
 
 download();
